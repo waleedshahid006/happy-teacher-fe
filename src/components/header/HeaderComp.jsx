717fcf394b83fdc67032bf7ff7fba99b78c636f0
waleed <waleedshahid006@gmail.com>
2021-09-27 22:24:31 +0500

    hamburger

--- src/components/header/HeaderComp.jsx
+++ src/components/header/HeaderComp.jsx
@@ -1,30 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Typography,
+  SwipeableDrawer,
 } from "@material-ui/core";
 
 import "../../App.css";
 import logo from "../../assets/logo.svg";
-
+import CloseIcon from '@mui/icons-material/Close';
+import MenuIcon from '@mui/icons-material/Menu';
 import useStyles from "./HeaderCompStyle";
 
 const HeaderComp = () => {
 
   const classes = useStyles();
+  const [open, setOpen] = useState();
 
   return (
-    <div style={{ display: 'flex', justifyContent:'center', paddingTop:'1%'}}>
-      <div style={{ display:'flex', flex:'0.3', justifyContent:'right'}}><img alt="logo" src={logo} width="232px" height="62px" /></div>
-      <div style={{ flex:'0.7'}}>
-        <div style={{ display: 'flex', justifyContent:'left', margin: ' 2% 0 0 15%'}}>
-        <Typography className={`${classes.navItems} poppins-regular`}> About </Typography>
-        <Typography className={`${classes.navItems} poppins-regular`}> Blog </Typography>
-        <Typography className={`${classes.navItems} poppins-regular`}> Podcast </Typography>
-        <Typography className={`${classes.navItems} poppins-regular`}> Shop </Typography>
-        <Typography className={`${classes.navItems} poppins-regular`}> Newsletter </Typography>
-        <Typography className={`${classes.navItems} poppins-regular`}> Contact </Typography>
+    <div className={classes.root}>
+      <div className={classes.logoDiv} >
+        <img alt="logo" src={logo} className={classes.logo} />
+      </div>
+      <div className={classes.navItemRoot} >
+        <div style={{ display: 'flex', justifyContent: 'left', margin: ' 2% 0 0 15%' }}>
+          <Typography className={`${classes.navItems} poppins-regular`}> About </Typography>
+          <Typography className={`${classes.navItems} poppins-regular`}> Blog </Typography>
+          <Typography className={`${classes.navItems} poppins-regular`}> Podcast </Typography>
+          <Typography className={`${classes.navItems} poppins-regular`}> Shop </Typography>
+          <Typography className={`${classes.navItems} poppins-regular`}> Newsletter </Typography>
+          <Typography className={`${classes.navItems} poppins-regular`}> Contact </Typography>
         </div>
       </div>
+      <div className={classes.hamburgerDiv}>
+        <MenuIcon className={classes.hamburger} onClick={() => setOpen(true)} />
+      </div>
+      <div >
+        <SwipeableDrawer
+          anchor="right"
+          open={open}
+          onClose={() => setOpen(false)}
+        >
+          <div className={classes.drawer}>
+            <CloseIcon className={classes.closeIcon} onClick={() => setOpen(false)} />
+            <div className={classes.drawerNavItemDiv}>
+              <Typography className={classes.drawerNavItem}> About </Typography>
+              <Typography className={classes.drawerNavItem}> Blogs </Typography>
+              <Typography className={classes.drawerNavItem}> Podcast </Typography>
+              <Typography className={classes.drawerNavItem}> Shops </Typography>
+              <Typography className={classes.drawerNavItem}> Newsletter </Typography>
+              <Typography className={classes.drawerNavItem}> Contact </Typography>
+            </div>
+          </div>
+        </SwipeableDrawer>
+      </div>
+
     </div>
   )
 }
--- src/components/header/HeaderCompStyle.js
+++ src/components/header/HeaderCompStyle.js
@@ -1,13 +1,71 @@
 import { makeStyles } from "@material-ui/core/styles";
+import bgHeroSection from "../../assets/bgHeroSection.png";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    paddingTop: '1%',
+  },
+  logoDiv: {
+    display: 'flex',
+    flex: '0.3',
+    justifyContent: 'right',
+  },
+  logo: {
+    width: '232px',
+    height: '62px',
+    [theme.breakpoints.down('sm')]: {
+      width: '170px',
+      height: '70px',
+    },
+  },
+  navItemRoot: {
+    flex: '0.7',
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
+  },
   navItems: {
     color: '#ffffff',
     fontSize: '20px !important',
     fontStyle: 'normal',
     fontWeight: 'normal !important',
     marginRight: '2%',
+  },
+  hamburgerDiv: {
+    display: 'none',
+    flex: '0.7',
+    [theme.breakpoints.down('sm')]: {
+      display: 'block',
+      textAlign: 'end'
+    },
+  },
+  hamburger: {
+    cursor: 'pointer !important',
+    fontSize: "40px !important",
+    color: '#FFFFFF !important',
+  },
+  drawer: {
+    backgroundImage: `url(${bgHeroSection})`,
+    height: '100vh',
+  },
+  drawerNavItemDiv: {
+    padding: '5em 4em 0 4em',
+    width: 'max-content',
+    textAlign: 'center',
+  },
+  drawerNavItem: {
+    fontSize: '22px',
+    marginBottom: '1.5em',
+    color:'#FFFFFF'
+  },
+  closeIcon:{
+    cursor: 'pointer !important',
+    fontSize: "40px !important",
+    float: 'right !important',
+    color: '#FFFFFF !important',
   }
-});
+}));
 
 export default useStyles;
